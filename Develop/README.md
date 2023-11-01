e698feeef4b6cacf3156f80e68943e8ea665474a
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-11-01 00:13:51 -0500

    got

--- Develop/README.md
+++ Develop/README.md
@@ -1,5 +1,23 @@
 
-# title
+# title 
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](function renderLicenseLink(license) {
+        if (license === "Apache 2.0 License"){
+                return 'https://opensource.org/licenses/Apache-2.0'
+        }
+        else if (license === "Boost Software License 1.0"){
+                return 'https://www.boost.org/LICENSE_1_0.txt'
+        }
+        else if (license === "BSD 3-Clause License"){
+                return 'https://opensource.org/licenses/BSD-3-Clause'
+        }
+        else if (license === "BSD 2-Clause License"){
+                return 'https://opensource.org/licenses/BSD-2-Clause'
+        }
+        else {return ''}
+        // let licLink;
+
+        // switch(license)
+})
 
 ## Description
 
@@ -34,10 +52,30 @@
 
 ## License
 
+        
         Apache 2.0 License
+        
 
 ## Badges
-[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+        [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](function renderLicenseLink(license) {
+        if (license === "Apache 2.0 License"){
+                return 'https://opensource.org/licenses/Apache-2.0'
+        }
+        else if (license === "Boost Software License 1.0"){
+                return 'https://www.boost.org/LICENSE_1_0.txt'
+        }
+        else if (license === "BSD 3-Clause License"){
+                return 'https://opensource.org/licenses/BSD-3-Clause'
+        }
+        else if (license === "BSD 2-Clause License"){
+                return 'https://opensource.org/licenses/BSD-2-Clause'
+        }
+        else {return ''}
+        // let licLink;
+
+        // switch(license)
+})
 
 ## Features
 
@@ -53,4 +91,5 @@
 
 ## Questions
 
+        Contact Me @:
         1
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+        if (license === "Apache 2.0 License"){
+                return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]('+renderLicenseLink+')'
+        } 
+        else if (license === "Boost Software License 1.0"){
+                return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]('+renderLicenseLink+')'
+        }
+        else if (license === "BSD 3-Clause License"){
+                return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]('+renderLicenseLink+')'
+        }
+        else if (license === "BSD 2-Clause License"){
+                return '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)]('+renderLicenseLink+')'
+        }
+        else {return ''}
+        // let bad = "";
+
+        // if (license != "None"){
+        //         bad = "!["
+        // }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+        if (license === "Apache 2.0 License"){
+                return 'https://opensource.org/licenses/Apache-2.0'
+        }
+        else if (license === "Boost Software License 1.0"){
+                return 'https://www.boost.org/LICENSE_1_0.txt'
+        }
+        else if (license === "BSD 3-Clause License"){
+                return 'https://opensource.org/licenses/BSD-3-Clause'
+        }
+        else if (license === "BSD 2-Clause License"){
+                return 'https://opensource.org/licenses/BSD-2-Clause'
+        }
+        else {return ''}
+        // let licLink;
+
+        // switch(license)
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+        return `
+        ${license}
+        `
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
 # ${data.nam}
+${renderLicenseBadge(data.lic)}
 
 ## Description
 
@@ -48,11 +89,11 @@ function generateMarkdown(data) {
 
 ## License
 
-        ${data.lic}
+        ${renderLicenseSection(data.lic)}
 
 ## Badges
 
-        ${bad}
+        ${renderLicenseBadge(data.lic)}
 
 ## Features
 
